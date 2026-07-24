f3e832659dd7df66e8cb447efd5a29b01821bc6a
xingjinshuang <xingjinshuang@ev-image.com>
2026-07-24 11:07:06 +0800

    @UPDATE CODE

--- src/main/java/com/tl/controller/AutomaticTowerArrangementController.java
+++ src/main/java/com/tl/controller/AutomaticTowerArrangementController.java
@@ -1,8 +1,15 @@
 package com.tl.controller;
 
+import com.tl.common.core.R;
+import com.tl.common.utils.LineGeometryUtils;
+import com.tl.domain.entity.LinePoint;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 自动塔式配置控制器
@@ -14,10 +21,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auto-tower-arrangement")
 public class AutomaticTowerArrangementController {
+    /**
+     * // 添加 logger
+     */
+    private final Logger log = LoggerFactory.getLogger(AutomaticTowerArrangementController.class);
 
 
-    // TODO: 2026/07/24 自动塔式
+    @RequestMapping("/auto-tower")
+    public void autoTowerArrangement() {
+        // 1. 获取线路数据
+        // 2. 获取塔型配置
+        // 3. 获取地形数据
+        // 4. 调用悬链线计算器计算悬链线
+        // 5. 根据悬链线结果生成塔位点
+        // 6. 返回塔位点结果
+    }
 
+    /**
+     * String line = "[112.653303,26.006398,370.5,112.655047,26.00851,333.1,112.656158,26.010697,397.1,112.659456,26.014043,507.2]";
+     *
+     * @param line
+     * @return {@link R }
+     */
+    @RequestMapping("/gen-tower-points")
+    public R genTowerPoints(String line) {
+        List<LinePoint> points = LineGeometryUtils.parseLine(line);
+        log.info("原始点:{}", points.size());
+        double length = LineGeometryUtils.lineLength(points);
+        log.info("线路长度:{}米", length);
+        List<LinePoint> samples = LineGeometryUtils.sampleLine(points, 5);
+        log.info("采样点:{}", samples.size());
+//        for (LinePoint p : samples) {
+//            log.info(String.valueOf(p));
+//        }
+        return R.ok(samples);
+    }
 
 
 }
