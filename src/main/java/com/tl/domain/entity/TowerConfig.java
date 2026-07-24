40d949fd713f29f5df0ba02b40205c40ce19c0b8
xingjinshuang <xingjinshuang@ev-image.com>
2026-07-24 11:44:57 +0800

    @UPDATE CODE

--- src/main/java/com/tl/domain/entity/TowerConfig.java
+++ src/main/java/com/tl/domain/entity/TowerConfig.java
@@ -4,48 +4,40 @@ import lombok.Data;
 
 @Data
 public class TowerConfig {
+    /**
+     * 采样间隔
+     */
+    public double sampleDistance = 5;
 
+    /**
+     * 最小档距
+     */
+    public double minSpan = 10;
 
-    //采样间隔
-    public double sampleDistance=5;
+    /**
+     * 最大档距
+     */
+    public double maxSpan = 300;
 
-
-    //最小档距
-    public double minSpan=10;
-
-
-    //最大档距
-    public double maxSpan=300;
-
-
-
-    //塔高
-    public double towerHeight=25;
-
-
-
-    //安全距离
-    public double groundClearance=6;
-
-
-
-    //最大坡度
-    public double maxSlope=45;
-
-
+    /**
+     * 塔高
+     */
+    public double towerHeight = 25;
 
     /**
      * 悬链线k
      */
-    public double k=0.02;
+    public double k = 0.02;
 
+    /**
+     * 最小对地距离
+     */
+    private double minGroundClearance = 6;
 
-    /** 最小对地距离 */
-    private double minimumGroundClearance = 6;
-
-    /** 最大允许坡度 */
-    private double maximumSlope = 45;
-
+    /**
+     * 最大允许坡度
+     */
+    private double maxSlope = 45;
 
 
 }
--- src/main/java/com/tl/domain/entity/TowerPoint.java
+++ src/main/java/com/tl/domain/entity/TowerPoint.java
@@ -6,12 +6,10 @@ import lombok.Data;
 @Data
 public class TowerPoint {
 
-
     /**
      * 塔号
      */
     public int id;
-
 
     /**
      * 经纬度
@@ -20,41 +18,30 @@ public class TowerPoint {
 
     public double lat;
 
-
-
     /**
      * 地面高程
      */
     public double elev;
-
-
 
     /**
      * 坡度
      */
     public double slope;
 
-
-
     /**
      * 塔高
      */
     public double towerHeight=25;
-
-
 
     /**
      * 与前塔距离
      */
     public double span;
 
-
-
     /**
      * 前后悬链线
      */
     public JSONArray catenary;
-
 
 
 }
