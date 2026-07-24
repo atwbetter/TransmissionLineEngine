bc8507a87a952b1a13c0b706f2b2358c12854138
xingjinshuang <xingjinshuang@ev-image.com>
2026-07-24 10:02:57 +0800

    @UPDATE CODE

---
+++ src/main/java/com/tl/controller/AutomaticTowerArrangementController.java
@@ -0,0 +1,23 @@
+package com.tl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 自动塔式配置控制器
+ *
+ * @author xingjinshuang
+ * @date 2026/07/24
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auto-tower-arrangement")
+public class AutomaticTowerArrangementController {
+
+
+    // TODO: 2026/07/24 自动塔式
+
+
+
+}
